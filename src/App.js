b6c73d7d4a7655d70f540c768f3a9256a432d0e6
Khoi01~ <nhhoangkhoi@gmail.com>
2022-10-09 09:40:40 +0700

    minor edit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 function App() {
   const [like, setLike] = useState(0);
-  return <h1> {like === 1 ? "&#128077" : "&#128078"}</h1>;
+  return <h1> {like === 1 ? "&#128077;" : "&#128078;"}</h1>;
 }
 
 export default App;
